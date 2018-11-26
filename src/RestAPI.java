7b1f13f20ae0582c775029b524e7c2b25353742a
antonyakovlev <anton2yakovlev@gmail.com>
2018-11-26 22:43:17 +0700

    add authorization

--- src/RestAPI.java
+++ src/RestAPI.java
@@ -1,7 +1,13 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.net.*;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.Base64;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 
 public class RestAPI {
@@ -52,9 +58,12 @@ public class RestAPI {
         }
     }
 
+    /*public String getAuthantication(String username, String password) {
+        String auth = new String(Base64.getEncoder().encode((username + ":" + password).getBytes());
+        return auth;
+    }*/
 
-
-    public static boolean putXML(String filename, String XML, String user, String pass){
+    public static boolean putXML2(String filename, String XML, String user, String pass){
         try {
             String authStr = user + ":"+pass;
             //byte [] authByteEnc = Base64.getEncoder().encode(authStr.getBytes());
@@ -80,6 +89,36 @@ public class RestAPI {
             return false;
         }
 
+    }
+
+
+
+    public static boolean putXML(String filename, String XML, String user, String pass) {
+        try {
+            String urlStr  =  "http://localhost:8080/exist/rest/"+filename;
+            URL url = new URL(urlStr);
+            HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+
+            String userCredentials = user+":"+pass;
+            String basicAuth = "Basic " + new String(Base64.getEncoder().encode(userCredentials.getBytes()));
+            httpCon.setRequestProperty ("Authorization", basicAuth);
+
+
+            httpCon.setDoOutput(true);
+            httpCon.setRequestMethod("PUT");
+
+            httpCon.setRequestProperty("Content-Type", "application/xml");
+
+            OutputStreamWriter out = new OutputStreamWriter(
+                    httpCon.getOutputStream());
+            out.write(XML);
+            out.close();
+            httpCon.getInputStream();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
 }
